3393c0c44fe1845b1328e160129528ba42044aae
JeanVittory <vittory.dev@gmail.com>
2025-02-20 22:53:13 -0500

    feat: :sparkles: Add function to interact with the LLM

--- nodes/chatbot.py
+++ nodes/chatbot.py
@@ -1,12 +1,43 @@
 from langchain_anthropic import ChatAnthropic
 from dotenv import load_dotenv
 from graph.graph_builder import graph_builder, State
+from langgraph.graph import START, END
 
 load_dotenv()
 
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
 
 def chatbot(state:State): 
-    return {"messages": [llm.invoke(state["messages"])]}
+    try:
+        return {"messages": [llm.invoke(state["messages"])]}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
 
 graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
